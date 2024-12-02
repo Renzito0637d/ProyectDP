1971e46a2ad5a299f9aa191f691ef50c820f2051
renzito0637d <renzito0637d@gmail.com>
2024-12-01 20:10:50 -0500

    Configurando encuestas

--- src/modelo/Encuesta.java
+++ src/modelo/Encuesta.java
@@ -4,10 +4,64 @@
  */
 package modelo;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author Renzo
  */
-public class Encuesta {
+public class Encuesta implements EstadoVariante{
+    private int idEncuesta;
+    private LocalDateTime fechaHoraLlenado;
+    private int estado;
+    private int respuesta1, respuesta2, respuesta3;
     
+    // Constructor
+    public Encuesta() {    }
+    public Encuesta(int idEncuesta, LocalDateTime fechaHoraLlenado, int estado, int respuesta1, int respuesta2, int respuesta3) {
+        this.idEncuesta = idEncuesta;
+        this.fechaHoraLlenado = fechaHoraLlenado;
+        this.estado = estado;
+        this.respuesta1 = respuesta1;
+        this.respuesta2 = respuesta2;
+        this.respuesta3 = respuesta3;
+    }  
+
+    // Encapsulamiento
+    public int getIdEncuesta() {        return idEncuesta;    }
+    public void setIdEncuesta(int idEncuesta) {        this.idEncuesta = idEncuesta;    }
+    public LocalDateTime getFechaHoraLlenado() {        return fechaHoraLlenado;    }
+    public void setFechaHoraLlenado(LocalDateTime fechaHoraLlenado) {        this.fechaHoraLlenado = fechaHoraLlenado;    }
+    public int getEstado() {        return estado;    }
+    public void setEstado(int estado) {        this.estado = estado;    }
+    public int getRespuesta1() {        return respuesta1;    }
+    public void setRespuesta1(int respuesta1) {        this.respuesta1 = respuesta1;    }
+    public int getRespuesta2() {        return respuesta2;    }
+    public void setRespuesta2(int respuesta2) {        this.respuesta2 = respuesta2;    }
+    public int getRespuesta3() {        return respuesta3;    }
+    public void setRespuesta3(int respuesta3) {        this.respuesta3 = respuesta3;    }
+
+    // Implementación de método abstracto
+    @Override
+    public String mostrarEstado() {
+        if (estado == INACTIVA) return "Inactiva";
+        if (estado == ACTIVA) return "Activa";
+        if (estado == COMPLETADA) return "Completada";
+        return null;
+    }
+    
+    // Métodos propios
+    public void setFechaHoraLlenadoActual() {
+        this.fechaHoraLlenado = LocalDateTime.now();
+    }
+    
+    public void activarEncuesta() {
+        estado = ACTIVA;
+    }
+    
+    public void completarEncuesta() {
+        estado = COMPLETADA;
+        setFechaHoraLlenadoActual();
+    }
+
 }
--- src/modelo/EstadoVariante.java
+++ src/modelo/EstadoVariante.java
@@ -5,7 +5,10 @@ public interface EstadoVariante {
     static final int EN_ESPERA = 1;
     static final int EN_TRAMITE = 2;
     static final int FINALIZADO = 3;
-    
+    // Constantes para estado de las encuestas de satisfaccion
+    static final int INACTIVA = 1;
+    static final int ACTIVA = 2;
+    static final int COMPLETADA = 3;
     // Método para mostrar el estado de la entidad implementada
     String mostrarEstado();
 }
